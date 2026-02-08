4d593f0666dc73f8dcf59c9a4bd29011fe70502b
LucaMarastoni <Marastoni311@gmail.com>
2026-02-08 23:46:10 +0100

    Refactor code structure for improved readability and maintainability

--- docs/boats-images.js
+++ docs/boats-images.js
@@ -3,10 +3,10 @@
   const base = isBoatDetail ? '../' : '';
 
   const map = {
-    'zar49': `${base}assets/img/FotoGommone/FotGommone1.jpg`,
-    'zar65': `${base}assets/img/FotoGommone/FotGommone6.jpg`,
-    'zar50-2025': `${base}assets/img/FotoGommone/FotGommone5.jpg`,
-    'zar53': `${base}assets/img/FotoGommone/FotGommone19.jpg`,
+    'zar49': `${base}assets/img/FotoGommone/49.jpg`,
+    'zar65': `${base}assets/img/FotoGommone/65.jpg`,
+    'zar50-2025': `${base}assets/img/30.jpg`,
+    'zar53': `${base}assets/img/FotoGommone/53.jpeg`,
   };
 
   window.BOAT_IMAGE_MAP = map;
